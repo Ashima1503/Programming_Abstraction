48137ec453e9eac144b85679b324ae57b2d3b3b1
Ashima1503 <78199579+Ashima1503@users.noreply.github.com>
2022-07-12 07:51:32 +0530

    Program to accept rollno, cgpa & gender of student

---
+++ roll_cgpa_gender.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int rollno;
+    cout<<"Enter rollno: ";
+    cin>>rollno;
+
+    float cgpa;
+    cout<<"Enter cgpa: ";
+    cin>>cgpa;
+
+    char gender;
+    cout<<"Enter gender: ";
+    cin>> gender;
+    
+    cout<<rollno<<endl;
+    cout<<cgpa<<endl;
+    cout<<gender<<endl;
+    
+    
+    return 0;
+}
