a6e7dca795a9e8a88ffdb07cb11f6559dabd2f60
Ashima1503 <78199579+Ashima1503@users.noreply.github.com>
2022-07-12 08:01:54 +0530

    Program to add 8% sales tax to a given amount.

---
+++ Sales_tax.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float amount1;
+    float amount2;
+    cin>>amount1;
+
+    amount2= amount1 + amount1*0.08;
+    cout<<amount2<<endl;
+    
+    return 0;
+}
