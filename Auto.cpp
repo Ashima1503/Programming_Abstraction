bc12aa54e19e29e7aeb9aeb863520448f2d44533
Ashima1503 <78199579+Ashima1503@users.noreply.github.com>
2022-07-13 07:49:52 +0530

    Learning concepts of Auto keyword.

---
+++ Auto.cpp
@@ -0,0 +1,17 @@
+//Auto keyword
+#include <iostream>
+using namespace std;
+void autoStorageClass(){
+    cout<<"Demonstarting auto class\n";
+    auto a=32;
+    auto b=3.2;
+    auto c="GeeksForGeeks";
+    auto d='G';
+   //printing the auto variables
+   cout<<a<<"\n"<<b<<"\n"<<c<<"\n"<<d<<"\n";
+}
+int main(){
+    //To demonstrate auto Storage Class
+    autoStorageClass();
+    return 0;
+}
